fb78da24468095e5c3d94913f5ad1d911eaf98a6
vitali <vitali.danichkin@gmail.com>
2017-03-07 16:19:26 +0600

    #8 changed header scroll size

--- vendor/sticky-header.js
+++ vendor/sticky-header.js
@@ -1,19 +1,29 @@
-var header = $('.header');
+// var header = $('.header');
+//
+//
+// function showSteackyHeader() {
+//     header.addClass('fixed');
+//
+//
+//
+// }
+// function hideSteackyHeader() {
+//     header.removeClass('fixed');
+//
+//
+// }
+// $(document).scroll(function () {
+//     var scrollSize = 100;
+//     var y = $(this).scrollTop();
+//     (y >= scrollSize) ? showSteackyHeader() : hideSteackyHeader();
+// });
 
 
-function showSteackyHeader() {
-    header.addClass('fixed');
-
-
-
-}
-function hideSteackyHeader() {
-    header.removeClass('fixed');
-
-
-}
-$(document).scroll(function () {
-    var scrollSize = 100;
-    var y = $(this).scrollTop();
-    (y >= scrollSize) ? showSteackyHeader() : hideSteackyHeader();
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 700){
+        $('header').addClass("fixed");
+    }
+    else{
+        $('header').removeClass("fixed");
+    }
 });
